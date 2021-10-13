a8b789c03aa2b67d386288765c0e91f12b7ec806
ABHISHEK UPADHYAY <abhisheku722@gmail.com>
2021-10-13 15:49:32 +0530

    commit inheritance

--- programs/CaseStudy.py
+++ programs/CaseStudy.py
@@ -1,43 +1,46 @@
 class customer:
-    def __init__(custID, custNAME):
-        def Display_details():
-            print(custID+" "+ custNAME)
-        @abstractmethod    
-        def CalculateAmount(interest, year):
-            return
-class RegCust(customer):
-    def __init__(custID, custNAME, Amount, custType):
+    def __init__(self,custID, custNAME):
         self.custID = custID
-        self.custName = cust
+        self.custName = custNAME
+
+
+    def Display_details(self,):
+        print(self.custID+" "+ self.custNAME)    
+    def CalculateAmount(interest, year):
+        pass
+class RegCust(customer):
+    def __init__(self,custID, custNAME, Amount, custType):
+        self.custID = custID
+        self.custName = custNAME
         self.custType = custType
         self.amount = Amount
 
-    def CalculateAmount(interest,year):
-        interest.p = principal
-        interest.r = rate
-        interest.t = time
+    def CalculateAmount(self,principal):
+        self.p = principal
+        self.r = 3.5
+        self.t = 3
 
-        interest = (p*r*t)/100
-        print(interest)
+        interest = (self.p*self.r*self.t)/100
+        print("Regular Customer Interest: ",interest)
 
 class PrivCust(customer):
-    def __init__(custID, custNAME, Amount, custType):
+    def __init__(self,custID, custNAME, Amount, custType):
         self.custID = custID
-        self.custName = cust
+        self.custName = custNAME
         self.custType = custType
-        self.Amount = amount
-    def CalculateAmount(interest,year):
-        interest.p = principal
-        interest.r = rate
-        interest.t = time
+        self.Amount = Amount
+    def CalculateAmount(self,principal):
+        self.p = principal
+        self.r = 3.5
+        self.t = 3
 
-        interest = (p*r*t)/100
-        print(interest)
+        interest = (self.p*self.r*self.t)/100
+        # print(interest)
         privamt = interest - 200
-        print(privamt)
+        print("Privileged Customer Interest Amount: ",privamt)
 
-regularcustomer = RegCust(12312, Raju Punjabi, 100, Regularcustomer)
-privileger = PrivCust(9999, Honey Singh, 100, Privileged Customer)
+regularcustomer = RegCust(12312, "Raju", 100, "Regularcustomer")
+privileger = PrivCust(9999," Honey", 100, "Privilegedcustomer")
 
-regularcustomer.CalculateAmount
-privileger.CalculateAmount
+regularcustomer.CalculateAmount(100)
+privileger.CalculateAmount(100)
